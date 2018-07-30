ef0630c3b7aeb25c2e4c2cad7946344a9e057087
Erika Linde <erika.j.linde@gmail.com>
2018-07-30 10:09:20 +0200

    Add project files.

---
+++ TheCoolestBooks/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheCoolestBooks.MvcApplication" Language="C#" %>
---
+++ TheCoolestBooks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheCoolestBooks.Startup))]
+namespace TheCoolestBooks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
